ac4234dde98d58490d1ec99a4598ad28f4fd0007
Michael Read <mike@twisted-grip.com>
2026-05-28 14:32:29 -0700

    adjust sleep

--- pulsar/init-pulsar.sh
+++ pulsar/init-pulsar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "sleeping for 18 seconds..."
-sleep 18s
+echo "sleeping for 15 seconds..."
+sleep 15s
 echo "tenants create tenant-a..."
 /pulsar/bin/pulsar-admin tenants create tenant-a
 echo "namespaces create tenant-a/entity-demo..."
